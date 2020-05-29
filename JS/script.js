4da0ce4fdacbea4b1411d0ad17d92a46f9ab27cb
Mateus Costa <mateuscosta_bh@hotmail.com>
2020-05-28 22:02:50 -0300

    ex 1

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+var pessoa = {
+  nome: ['Bob', 'Smith'],
+  idade: 32,
+  sexo: 'masculino',
+  interesses: ['música', 'esquiar'],
+  bio: function() {
+    alert(this.nome[0] + ' ' + this.nome[1] + ' tem ' + this.idade + ' anos de idade. Ele gosta de ' + this.interesses[0] + ' e ' + this.interesses[1] + '.');
+  },
+  saudacao: function() {
+    alert('Oi! Eu sou ' + this.nome[0] + '.');
+  }
+};
