e62d76d48a2b58068de8f748309b81b59973114a
Mateus Costa <mateuscosta_bh@hotmail.com>
2020-06-25 22:20:31 -0300

    programa desse jeito memsmo

---
+++ prova d3/ex_7.php
@@ -0,0 +1,52 @@
+<?php
+class SuperHeroi {
+public function Heroi($id, $nome, $poderEspecial, $energia, $forca, $origem ){
+    $this -> setid($id);
+    $this -> setnome($nome);
+    $this -> setpoderEspecial($poderEspecial);
+    $this -> setenergia($energia);
+    $this -> setforca($forca);
+    $this -> setorigem($origem);
+}
+public function getid()
+{
+    return $this ->id ;
+}
+public function setid($id)
+{
+    $this ->id = $id;
+}
+public function getid()
+{
+    return $this ->id;
+}
+public function setid()
+{
+    return $this ->id ;
+}
+public function poderEspecial()
+{ 
+    return $this ->poderEspecial;
+}
+public function getid(){
+    return $this ->id;
+}
+public function setid($id)
+$this ->id = $id
+public function getpoderEspecial(){
+    return $this ->poderEspecial;
+}
+
+
+
+$n1 = new SuperHeroi ("1", "Son Goku", "kaioken","4000","8100","planeta vegeta")
+$n1 = new SuperHeroi ("2", "Kuririn", "kaioken","1500","4000","planeta Terra")
+$n1 = new SuperHeroi ("1", "Harry Potter", "magica","3000","5000","terra")
+$n1 = new SuperHeroi ("1", "honey wesley", "magica","2000","3500","terra")
+
+public function aumentarEnergia($aumentarEnergia){
+    $this -> $energia*11517928;}
+    public function calculopoder($calculopoder){
+    $this -> $energia/$forca;}
+}
+?>
